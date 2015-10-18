441233766855a1493ffa7718825b28cb0dee6f0e
Leso_KN <info@lesosoftware.com>
2015-10-18 23:16:31 +0200

    Timeshift & seconds
    Corrected the timeshift and added a seconds view

--- src/Main.c
+++ src/Main.c
@@ -2,30 +2,30 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_time_layer_seconds;
 
 static void update_time() {
   // Get a tm structure
-<<<<<<< HEAD
-  time_t temp = time(NULL);
-=======
-  time_t temp = time(NULL) - 360000000;
->>>>>>> origin/Mobile-Development
+  time_t temp = time(NULL) - 180;
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "00:00";
+  static char buffer[] = "00:00 ";
+  static char bufferS[] = "00";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(buffer, sizeof("00:00 "), "%H:%M", tick_time);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(buffer, sizeof("00:00 "), "%I:%M", tick_time);
   }
+  strftime(bufferS, sizeof("00"), "%S", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_time_layer_seconds, bufferS);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -35,6 +35,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_load(Window *window) {
   // Create Background
   s_time_layer = text_layer_create(GRect(0, 0, 144, 168));
+  s_time_layer_seconds = text_layer_create(GRect(0, 145, 144, 168));
   text_layer_set_background_color(s_time_layer, GColorBlack);
 
   // Add it as a child layer to the Window's root layer
@@ -43,16 +44,21 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorClear);
+  text_layer_set_background_color(s_time_layer_seconds, GColorBlack);
+  text_layer_set_text_color(s_time_layer_seconds, GColorClear);
   text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_text(s_time_layer_seconds, "00");
 
   // Improve the layout to be more like a watchface
   GFont s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SEGOE_UI_NORMAL_48));
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_time_layer_seconds, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer_seconds));
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
   update_time();
 }
 
